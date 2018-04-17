d3d6581207a52d7c053ac86e0093d1acba47e09d
Carlos Lopez <carloslopeznieto13@gmail.com>
2018-04-17 16:49:56 +0200

    modificaciones modo de mostrar los datos

--- src/app/services/http-services.service.ts
+++ src/app/services/http-services.service.ts
@@ -3,11 +3,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from '@firebase/util';
 import { ToastrService } from 'ngx-toastr';
 import { HttpListComponent } from '../components/http-list/http-list.component';
+import { Product } from '../models/product'
 
 @Injectable()
 export class HttpServicesService {
 
   public posts: Post[];
+  public products: Product[];
+  public peticion: Peticion[];
 
   constructor(
     public http: HttpClient, private toastr: ToastrService
@@ -16,24 +19,21 @@ export class HttpServicesService {
   configUrl = '192.168.0.1';
 
   public getPosts() {
-    //let headers = new HttpParams().set('Authorization','auth-token');
-    //this.posts = this.http.get('http://192.168.1.65:8084/product/new?name=aswerd&category=regdf&location=qwe&price=2');
+    console.log(this.http.get('http://192.168.1.65:8084/product/new?name=nombre&category=book&location=esp&price=20'));
+
     this.http.get<Post[]>
       ('https://jsonplaceholder.typicode.com/posts').subscribe(x => {
         this.posts = x;
-        console.log('Number of posts: ' + this.posts.length);
+        //console.log('Number of posts: ' + this.posts.length);
 
         var i = 1;
 
         for (let index = 0; index < this.posts.length; index++) {
-          console.log('Id: ' + this.posts[index].id);
-          console.log('Title: ' + this.posts[index].title);
-          console.log('Body: ' + this.posts[index].body);
 
           var table: HTMLTableElement = <HTMLTableElement>document.getElementById("tablecontent");
 
           
-          if (table.rows.length <= i) {
+          if (true/*table.rows.length <= i*/) {
 
             var row = table.insertRow(table.rows.length);
 
@@ -62,11 +62,39 @@ export class HttpServicesService {
 
   }
 
+  httpPostExample() {
+
+    this.http.post("http://192.168.1.65:8084/product",
+        {
+            "name": "prueba",
+            "category": "prueba2",
+            "location": "prueba3",
+            "price": 23
+        })
+        .subscribe(
+            (val) => {
+                console.log("POST call successful value returned in body", 
+                            val);
+            },
+            response => {
+                console.log("POST call in error", response);
+            },
+            () => {
+                console.log("The POST observable is now completed.");
+            });
+    }
+
 }
+
+
 
 interface Post {
   userId: number,
   id: number,
   title: string,
   body: string,
+}
+
+interface Peticion {
+  content: string
 }
